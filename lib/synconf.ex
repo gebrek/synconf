d4f9d2a4b706fd617cd129f77789adbff85f432c
Jacob Sonnenberg <jacobsonnenberg0@gmail.com>
2016-06-25 21:40:59 -0500

    update function

--- lib/synconf.ex
+++ lib/synconf.ex
@@ -25,14 +25,34 @@ defmodule Conf do
     end)
   end
 
+  def get(conf) do
+    Agent.get(conf, &(&1))
+  end
+
   def update?(conf) do
-    c = Agent.get(conf, &(&1))
+    c = get(conf)
     {:ok, stat} = File.stat(c.path)
     if stat.mtime > c.versions[c.head].timestamp do
       {:ok, content} = File.read(c.path)
       :crypto.hash(:sha, content) != c.head
     else
       false
+    end
+  end
+
+  def update(conf) do
+    if update?(conf) do
+      c = get(conf)
+      {:ok, content} = File.read(c.path)
+      {:ok, stat} = File.stat(c.path)
+      chksum = :crypto.hash(:sha, content)
+      Agent.update(conf, fn(c) -> %Conf{path: c.path,
+					versions: Map.put(c.versions, chksum,
+					  %Ver{content: content,
+					       parent: c.head,
+					       timestamp: stat.mtime}),
+					head: chksum}
+      end)
     end
   end
 end
